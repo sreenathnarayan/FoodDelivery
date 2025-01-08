f461d455dd9257c3e2e73508a1d2dbba80f29b02
Sreenath Narayan <kvsreenath007@gmail.com>
2025-01-08 23:14:48 +0530

    four Module Implemented for food delivery application

--- UserService/src/main/java/com/food/UserServiceApplication.java
+++ UserService/src/main/java/com/food/UserServiceApplication.java
@@ -1,13 +1,11 @@
-package com.food.delivery;
+package com.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodDeliveryApplication {
-
+public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FoodDeliveryApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class,args);
     }
-
 }
